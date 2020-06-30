ed45c8e4cf830d51cedd0ca7737b13725039713f
allen44 <allen44@users.noreply.github.com>
2020-06-29 14:20:02 -1000

    Update brain.py

--- brain.py
+++ brain.py
@@ -14,39 +14,50 @@ except:
 #Initialize the Model
 from imageai.Prediction import ImagePrediction
 result_count = 5
-prediction = ImagePrediction()
+try:
+    prediction = ImagePrediction()
+    prediction.setModelTypeAsSqueezeNet()
+    prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+    prediction.loadModel()
+    predictions, probabilities = prediction.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
+    print(f"\n\nThe SqueezeNet model's top {result_count} predictions with probability: ")
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
+except:
+    print("Squeezenet didn't work...") 
 
-prediction.setModelTypeAsSqueezeNet()
-prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
-prediction.loadModel()
-predictions, probabilities = prediction.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
-print(f"The SqueezeNet model's top {result_count} predictions with probability: ")
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction , " : " , eachProbability)
+try:
+    prediction2 = ImagePrediction()
+    prediction2.setModelTypeAsResNet()
+    prediction2.setModelPath(os.path.join(execution_path, "resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
+    prediction2.loadModel()
+    predictions2, probabilities2 = prediction2.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
+    print(f"\n\nThe ResNet model's top {result_count} predictions with probability: ")
+    for eachPrediction2, eachProbability2 in zip(predictions2, probabilities2):
+        print(eachPrediction2 , " : " , eachProbability2)
+except:
+    print("ResNet didn't work...")
 
-prediction2 = ImagePrediction()
-prediction2.setModelTypeAsResNet()
-prediction2.setModelPath(os.path.join(execution_path, "resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
-prediction2.loadModel()
-predictions2, probabilities2 = prediction2.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
-print(f"The ResNet model's top {result_count} predictions with probability: ")
-for eachPrediction2, eachProbability2 in zip(predictions2, probabilities2):
-    print(eachPrediction2 , " : " , eachProbability2)
+try:
+    prediction3 = ImagePrediction()
+    prediction3.setModelTypeAsInceptionV3()
+    prediction3.setModelPath(os.path.join(execution_path, "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"))
+    prediction3.loadModel()
+    predictions3, probabilities3 = prediction3.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
+    print(f"\n\nThe Inception model's top {result_count} predictions with probability: ")
+    for eachprediction3, eachProbability3 in zip(predictions3, probabilities3):
+        print(eachprediction3 , " : " , eachProbability3)
+except:
+    print("Inception didn't work...")
 
-prediction3 = ImagePrediction()
-prediction3.setModelTypeAsInceptionV3()
-prediction3.setModelPath(os.path.join(execution_path, "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"))
-prediction3.loadModel()
-predictions3, probabilities3 = prediction3.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
-print(f"The Inception model's top {result_count} predictions with probability: ")
-for eachprediction3, eachProbability3 in zip(predictions3, probabilities3):
-    print(eachprediction3 , " : " , eachProbability3)
-
-# prediction4  = ImagePrediction()
-# prediction4.setModelTypeAsDenseNet()
-# prediction4.setModelPath(os.path.join(execution_path, "DenseNet-BC-121-32.h5"))
-# prediction4.loadModel()
-# predictions4, probabilities4 = prediction3.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
-# print(f"The DenseNet model's top {result_count} predictions with probability: ")
-# for eachprediction4, eachProbability4 in zip(predictions4, probabilities4):
-#     print(eachprediction4 , " : " , eachProbability4)
+try:
+    prediction4  = ImagePrediction()
+    prediction4.setModelTypeAsDenseNet()
+    prediction4.setModelPath(os.path.join(execution_path, "DenseNet-BC-121-32.h5"))
+    prediction4.loadModel()
+    predictions4, probabilities4 = prediction3.predictImage(os.path.join(execution_path, image_filename), result_count=result_count )
+    print(f"\n\nThe DenseNet model's top {result_count} predictions with probability: ")
+    for eachprediction4, eachProbability4 in zip(predictions4, probabilities4):
+        print(eachprediction4 , " : " , eachProbability4)
+except:
+    print("DenseNet didn't work...")

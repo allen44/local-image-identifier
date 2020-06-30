4e8f60dde838a348bdc25b80361d6d154e1a93f6
allen44 <allen44@users.noreply.github.com>
2020-06-29 17:07:31 -1000

    Tested and verified on more images
    Densenet model requires Tensorflow <2.0, ands Tensorflow <2 requires Python <3.8.

--- brain.py
+++ brain.py
@@ -4,12 +4,6 @@ import os
 execution_path = os.getcwd()
 print(execution_path)
 
-#Add your own photo to identify
-from sys import argv
-try:
-    image_filename = argv[1]
-except:
-    image_filename = 'giraffe.jpg'
 
 #Models folder
 models_folder = os.environ["USERPROFILE"] + "\\Models\\"
@@ -21,78 +15,117 @@ print(f"models_folder: {models_folder}")
 print(f"models_abs_paths: {models_abs_paths}")
 
 #Photos folder
-image_folder = execution_path
+image_folder = execution_path 
 allfiles = os.listdir(image_folder)
-images_files = []
+image_files = []
 for eachPicture in allfiles:
     if eachPicture.endswith(".png") or eachPicture.endswith(".jpg"):
-        images_files.append(eachPicture)
-print(images_files)
+        image_files.append(eachPicture)
+print(image_files)
 
 #Initialize the Model
 from imageai.Prediction import ImagePrediction
-result_count = 1
+result_count = 5
+
+#Initializxe a text file to write to
+def write_to_txt(line):
+    with open('results.txt', mode='a') as output:
+        output.write(line)
 
 for model_abs_path in models_abs_paths:
+    multiple_prediction = ImagePrediction()
     if 'squeezenet' in model_abs_path.lower():
         try:
-            prediction = ImagePrediction()
-            prediction.setModelTypeAsSqueezeNet()
-            prediction.setModelPath(model_abs_path)
-            prediction.loadModel()
-            for image_file in images_files:
-                predictions, probabilities = prediction.predictImage(os.path.join(execution_path, image_file), result_count=result_count )
-                print(f"\n\nThe SqueezeNet model's top {result_count} predictions with probability for {image_file}: ")
-                for eachPrediction, eachProbability in zip(predictions, probabilities):
-                    print(eachPrediction , " : " , eachProbability)
+            multiple_prediction.setModelTypeAsSqueezeNet()
+            multiple_prediction.setModelPath(model_abs_path)
+            multiple_prediction.loadModel()
+            results_array = multiple_prediction.predictMultipleImages(image_files, result_count_per_image=result_count)
+            # print(f"results_array: {results_array}")
+            results_array_idx = 0
+            for each_result in results_array:
+                predictions, percentage_probabilities = each_result["predictions"], each_result["percentage_probabilities"]
+                line = (f"\n\nThe SqueezeNet model's top {result_count} predictions with probability for {image_files[results_array_idx]}: ")
+                print(line, end='')
+                write_to_txt(line)
+                for index in range(len(predictions)):
+                    line = f"\n{predictions[index]}: {percentage_probabilities[index]}"
+                    print(line, end='')
+                    write_to_txt(line)
+                print("-----------------------")
+                results_array_idx = results_array_idx + 1
         except Exception as e:
             print("\n\nSqueezeNet didn't work.")
             print(e)
             print("Next model..")
     elif 'resnet' in model_abs_path.lower():
         try:    
-            prediction2 = ImagePrediction()
-            prediction2.setModelTypeAsResNet()
-            prediction2.setModelPath(model_abs_path)
-            prediction2.loadModel()
-            for image_file in images_files:
-                predictions2, probabilities2 = prediction2.predictImage(os.path.join(execution_path, image_file), result_count=result_count )
-                print(f"\n\nThe ResNet model's top {result_count} predictions with probability for {image_file}: ")
-                for eachPrediction2, eachProbability2 in zip(predictions2, probabilities2):
-                    print(eachPrediction2 , " : " , eachProbability2)
+            multiple_prediction.setModelTypeAsResNet()
+            multiple_prediction.setModelPath(model_abs_path)
+            multiple_prediction.loadModel()
+            results_array = multiple_prediction.predictMultipleImages(image_files, result_count_per_image=result_count)
+            # print(f"results_array: {results_array}")
+            results_array_idx = 0
+            print(f"\n\nThe ResNet model's top {result_count} predictions with probability for each image:")
+            for each_result in results_array:
+                predictions, percentage_probabilities = each_result["predictions"], each_result["percentage_probabilities"]
+                line = (f"\n\nThe ResNet model's top {result_count} predictions with probability for {image_files[results_array_idx]}: ")
+                print(line, end='')
+                write_to_txt(line)
+                for index in range(len(predictions)):
+                    line = f"\n{predictions[index]}: {percentage_probabilities[index]}"
+                    print(line, end='')
+                    write_to_txt(line)
+                print("-----------------------")
+                results_array_idx = results_array_idx + 1
         except Exception as e:
             print("\n\n\ResNet didn't work.")
             print(e)
             print("Next model..")
     elif 'inception' in model_abs_path.lower():
         try:
-            prediction3 = ImagePrediction()
-            prediction3.setModelTypeAsInceptionV3()
-            prediction3.setModelPath(model_abs_path)
-            prediction3.loadModel()
-            for image_file in images_files:
-                predictions3, probabilities3 = prediction3.predictImage(os.path.join(execution_path, image_file), result_count=result_count )
-                print(f"\n\nThe Inception model's top {result_count} predictions with probability for {image_file}: ")
-                for eachprediction3, eachProbability3 in zip(predictions3, probabilities3):
-                    print(eachprediction3 , " : " , eachProbability3)
+            multiple_prediction.setModelTypeAsInceptionV3()
+            multiple_prediction.setModelPath(model_abs_path)
+            multiple_prediction.loadModel()
+            results_array = multiple_prediction.predictMultipleImages(image_files, result_count_per_image=result_count)
+            # print(f"results_array: {results_array}")
+            results_array_idx = 0
+            for each_result in results_array:
+                predictions, percentage_probabilities = each_result["predictions"], each_result["percentage_probabilities"]
+                line = (f"\n\nThe Inception model's top {result_count} predictions with probability for {image_files[results_array_idx]}: ")
+                print(line, end='')
+                write_to_txt(line)
+                for index in range(len(predictions)):
+                    line = f"\n{predictions[index]}: {percentage_probabilities[index]}"
+                    print(line, end='')
+                    write_to_txt(line)
+                print("-----------------------")
+                results_array_idx = results_array_idx + 1
         except Exception as e:
             print("\n\ninception didn't work.")
             print(e)
             print("Next model..")
-    elif 'densenet' in model_abs_path.lower():
-        try:
-            prediction4  = ImagePrediction()
-            prediction4.setModelTypeAsDenseNet()
-            prediction4.setModelPath(model_abs_path)
-            prediction4.loadModel()
-            for image_file in images_files:
-                predictions4, probabilities4 = prediction3.predictImage(os.path.join(execution_path, image_file), result_count=result_count )
-                print(f"\n\nThe DenseNet model's top {result_count} predictions with probability for {image_file}: ")
-                for eachprediction4, eachProbability4 in zip(predictions4, probabilities4):
-                    print(eachprediction4 , " : " , eachProbability4)
-        except Exception as e:
-            print("\n\nDenseNet didn't work.")
-            print(e)
-            print("Next model..")
+    # # elif 'densenet' in model_abs_path.lower():
+    #     try:
+    #         multiple_prediction.setModelTypeAsDenseNet()
+    #         multiple_prediction.setModelPath(model_abs_path)
+    #         multiple_prediction.loadModel()
+    #         results_array = multiple_prediction.predictMultipleImages(image_files, result_count_per_image=result_count)
+    #         # print(f"results_array: {results_array}")
+    #         results_array_idx = 0
+    #         for each_result in results_array:
+    #             predictions, percentage_probabilities = each_result["predictions"], each_result["percentage_probabilities"]
+    #             line = (f"\n\nThe SqueezeNet model's top {result_count} predictions with probability for {image_files[results_array_idx]}: ")
+    #             print(line, end='')
+    #             write_to_txt(line)
+    #             for index in range(len(predictions)):
+                    # line = f"\n{predictions[index]}: {percentage_probabilities[index]}"
+                    # print(line, end='')
+                    # write_to_txt(line)
+    #             print("-----------------------")
+    #             results_array_idx = results_array_idx + 1
+    #     except Exception as e:
+    #         print("\n\nDenseNet didn't work.")
+    #         print(e)
+    #         print("Next model..")
     else:
         print('no models worked...')
